ce923cf487b723001f33b08b66570fb11e705f73
Mrszew <marcelraszewski@gmail.com>
2025-07-03 10:13:05 +0200

    test: check only for 'Marcel Raszewski' in footer (fix typo)

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,3 @@
-describe('Plik index.html', () => {
-  it('powinien zawierać słowo "Marcela Raszewskiego"', () => {
-    cy.readFile('app/public/index.html').should('include', 'Marcela Raszewskiego');
-  });
-});
-
 describe('Stopka', () => {
   it('powinna zawierać imię i nazwisko', () => {
     cy.readFile('app/public/index.html').should('include', 'Marcel Raszewski');
